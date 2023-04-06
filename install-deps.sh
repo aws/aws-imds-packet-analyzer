bc1e4c28432600969a609578afdaf5c5ee47852e
Gean Boegman <geanb@amazon.com>
2023-04-06 10:11:27 +0200

    Clean up error message.

--- install-deps.sh
+++ install-deps.sh
@@ -8,9 +8,8 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 if ! grep -q "Amazon Linux 2" /etc/os-release; then
-  echo "[ERROR] Only Amazon Linux 2 is support at this time."
-  echo "[ERROR] Other distro support possible, needs bcc and python-bcc"
-  exit 1
+  echo "[ERROR] Only Amazon Linux 2 supported at this time."
+  echo "[ERROR] For other linux flavours you will need to manually install bcc and python-bcc"  exit 1
 fi
 
 # amazon-linux-extras cache uses sudo_user's homedir if called with sudo or /var/cache if directly run as root
